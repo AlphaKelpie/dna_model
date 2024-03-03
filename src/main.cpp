c818cf177e2e5385c32e49123f4dea4802ff6380
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-03 18:50:42 +0100

    Metropolis algorithm implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <vector>
 #include <fstream>
 #include <iomanip>
+#include <random>
 
+#include "tqdm.hpp"
 #include "coordinates.hpp"
 #include "base.hpp"
 
 // using Eigen::MatrixXd;
 
+// evolution steps
+int constexpr epochs = 100000;
+// step size
+double constexpr step = 0.017453292519943295;
+
 // number of basis
-int constexpr n = 500;
+int constexpr n = 100;
 // constant for bonding energy (pN/nm)
 double constexpr k_bond = 1000;
 // constant for bending energy (pN*nm)
@@ -17,8 +24,15 @@ double constexpr k_bend = 7000;
 double constexpr l_0 = 0.705383591565685;
 // equilibrium angle between basis of the same elix (radiants) (~31.4d)
 double constexpr theta_0 = 0.5483452644857695;
+// Boltzman constant (pN*nm/K)
+double constexpr k_b = 1.380649e-2;
+// temperature (K)
+double constexpr T = 300.;
 
-
+// probability function
+double p(double const& now, double const& before) {
+  return std::exp(-(now - before) / (k_b * T));
+}
 
 template <typename T>
 double bonding_energy(Coordinates<T> const& now, Coordinates<T> const& before) {
@@ -34,7 +48,7 @@ double bending_energy(Coordinates<T> const& now, Coordinates<T> const& before,
 }
 
 template <typename T>
-double energy(std::vector<Base<T>> const& dna) {
+double calculate_energy(std::vector<Base<T>> const& dna) {
   double energy = 0.;
   for (int i = 1; i != n; ++i) {
     // bonding energy
@@ -49,6 +63,11 @@ double energy(std::vector<Base<T>> const& dna) {
 }
 
 int main() {
+  // std::random_device rd;
+  // std::mt19937 gen(rd());
+  std::mt19937 gen(0);
+  std::uniform_int_distribution<int> oscillation(-1, 1);
+  std::uniform_real_distribution<double> uniform(0., 1.);
   std::cout << std::fixed << std::setprecision(6);
   std::vector<Base<double>> dna;
   dna.push_back(Base<double>(0., 0., 0.));
@@ -58,6 +77,7 @@ int main() {
   dna[1].calculate_coordinates(previous_rotation_matrix,
                                central_previous_coordinates);
   central_previous_coordinates = dna[1].central();
+
   for (int i = 2; i != n; ++i) {
     Base<double> b(0., 0., i*psi_0);
     previous_rotation_matrix = b.calculate_coordinates(previous_rotation_matrix,
@@ -65,13 +85,28 @@ int main() {
     central_previous_coordinates = b.central();
     dna.push_back(b);
   }
-  // for (auto const& b : dna) {
-  //   b.central().print();
-  //   b.p().print();
-  //   b.q().print();
-  //   std::cout << "\n\n";
-  // }
-  std::cout << energy(dna) << '\n';
+  double energy = calculate_energy(dna);
+
+  for (int e : tq::trange(epochs)) {
+    std::vector<Base<double>> dna_new{dna[0], dna[1]};
+    previous_rotation_matrix = MatrixXd::Identity(3, 3);
+    central_previous_coordinates = dna_new[1].central();
+    for (int i = 2; i != n; ++i) {
+      double const theta = dna[i].theta() + oscillation(gen) * step;
+      double const phi = dna[i].phi() + oscillation(gen) * step;
+      double const psi = dna[i].psi() + oscillation(gen) * step;
+      Base<double> b(theta, phi, psi);
+      previous_rotation_matrix = b.calculate_coordinates(previous_rotation_matrix,
+                                      central_previous_coordinates);
+      central_previous_coordinates = b.central();
+      dna_new.push_back(b);
+    }
+    double const energy_new = calculate_energy(dna_new);
+    if (p(energy_new, energy) > uniform(gen)) {
+      dna = std::move(dna_new);
+      energy = energy_new;
+    }
+  }
 
   // Save central, p, and q coordinates of Base in separate files
   std::ofstream c_file("./file_c.txt");
