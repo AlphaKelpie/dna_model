a0e7ca9e72e8178a77e28d75970f100c84c5bb36
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-03 17:44:47 +0100

    now it is easyer to set different angles creating a Base. Energy correction

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 // using Eigen::MatrixXd;
 
 // number of basis
-int constexpr n = 11;
+int constexpr n = 500;
 // constant for bonding energy (pN/nm)
 double constexpr k_bond = 1000;
 // constant for bending energy (pN*nm)
@@ -36,10 +36,10 @@ double bending_energy(Coordinates<T> const& now, Coordinates<T> const& before,
 template <typename T>
 double energy(std::vector<Base<T>> const& dna) {
   double energy = 0.;
-  for (short int i = 1; i != n; ++i) {
+  for (int i = 1; i != n; ++i) {
     // bonding energy
-    energy = bonding_energy<T>(dna[i].p(), dna[i-1].p())
-           + bonding_energy<T>(dna[i].q(), dna[i-1].q());
+    energy += bonding_energy<T>(dna[i].p(), dna[i-1].p())
+            + bonding_energy<T>(dna[i].q(), dna[i-1].q());
     // bending energy
     if (i == n-1) { continue; }
     energy += bending_energy<T>(dna[i].p(), dna[i-1].p(), dna[i+1].p())
@@ -49,23 +49,28 @@ double energy(std::vector<Base<T>> const& dna) {
 }
 
 int main() {
+  std::cout << std::fixed << std::setprecision(6);
   std::vector<Base<double>> dna;
   dna.push_back(Base<double>(0., 0., 0.));
-  Coordinates<double> central_previous_coordinates = {0, 0, 0};
+  dna.push_back(Base<double>(0., 0., psi_0));
+  Coordinates<double> central_previous_coordinates = dna[0].central();
   MatrixXd previous_rotation_matrix = MatrixXd::Identity(3, 3);
-  for (short int i = 1; i != n; ++i) {
+  dna[1].calculate_coordinates(previous_rotation_matrix,
+                               central_previous_coordinates);
+  central_previous_coordinates = dna[1].central();
+  for (int i = 2; i != n; ++i) {
     Base<double> b(0., 0., i*psi_0);
     previous_rotation_matrix = b.calculate_coordinates(previous_rotation_matrix,
                                     central_previous_coordinates);
     central_previous_coordinates = b.central();
     dna.push_back(b);
   }
-  for (auto const& b : dna) {
-    b.central().print();
-    b.p().print();
-    b.q().print();
-    std::cout << "\n\n";
-  }
+  // for (auto const& b : dna) {
+  //   b.central().print();
+  //   b.p().print();
+  //   b.q().print();
+  //   std::cout << "\n\n";
+  // }
   std::cout << energy(dna) << '\n';
 
   // Save central, p, and q coordinates of Base in separate files
@@ -74,9 +79,9 @@ int main() {
   std::ofstream q_file("./file_q.txt");
 
   if (c_file.is_open() && p_file.is_open() && q_file.is_open()) {
-    c_file << std::fixed << std::setprecision(4);
-    p_file << std::fixed << std::setprecision(4);
-    q_file << std::fixed << std::setprecision(4);
+    c_file << std::fixed << std::setprecision(6);
+    p_file << std::fixed << std::setprecision(6);
+    q_file << std::fixed << std::setprecision(6);
     for (auto const& b : dna) {
       c_file << b.central().x_ << '\t' << b.central().y_ << '\t' << b.central().z_ << '\n';
       p_file << b.p().x_ << '\t' << b.p().y_ << '\t' << b.p().z_ << '\n';
