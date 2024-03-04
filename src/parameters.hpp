058ce7e01827b2ce8b48f281ff1c25bdedc2dd92
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-04 13:49:32 +0100

    order parameters and split in groups

--- src/parameters.hpp
+++ src/parameters.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+// parameters to create DNA
 // vertical distance between basis (nm)
 double constexpr base = 0.34;
 // distance between elix and backbone structure (nm)
@@ -8,17 +9,23 @@ double constexpr sigma_0 = 1.;
 double constexpr psi_0 = 0.6283185307179586;
 // angle between P and Q elixes (radiants) (~127.06d)
 double constexpr omega_0 = 2.217594814298678;
+
+// parameters to calculate energy
 // constant for bonding energy (pN/nm)
 double constexpr k_bond = 1000;
-// constant for bending energy (pN*nm)
-double constexpr k_bend = 7000;
 // equilibrium distance between basis of the same elix (nm)
 double constexpr l_0 = 0.705383591565685;
+// constant for bending energy (pN*nm)
+double constexpr k_bend = 7000;
 // equilibrium angle between basis of the same elix (radiants) (~31.4d)
 double constexpr theta_0 = 0.5483452644857695;
+
+// physical constants
 // Boltzman constant (pN*nm/K)
 double constexpr k_b = 1.380649e-2;
 // temperature (K)
 double constexpr T = 300.;
+
+// parameters to Metropolis algorithm
 // step size
 double constexpr step = 0.017453292519943295;
