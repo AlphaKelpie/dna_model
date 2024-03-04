12a1b70619abfa4a1015ececb4e43d12552786e7
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-04 10:29:50 +0100

    add save_coordinates() functions
    it saves coordinates of a bases' vector in three txt files

--- src/functions.hpp
+++ src/functions.hpp
@@ -27,14 +27,39 @@ double bending_energy(Coordinates<T> const& now, Coordinates<T> const& before,
 template <typename T>
 double calculate_energy(std::vector<Base<T>> const& dna) {
   double energy = 0.;
-  for (int i = 1; i != n; ++i) {
+  int const m = static_cast<int>(dna.size());
+  for (int i = 1; i != m; ++i) {
     // bonding energy
     energy += bonding_energy<T>(dna[i].p(), dna[i-1].p())
             + bonding_energy<T>(dna[i].q(), dna[i-1].q());
     // bending energy
-    if (i == n-1) { continue; }
+    if (i == m-1) { continue; }
     energy += bending_energy<T>(dna[i].p(), dna[i-1].p(), dna[i+1].p())
             + bending_energy<T>(dna[i].q(), dna[i-1].q(), dna[i+1].q());
   }
   return energy;
+}
+
+// Save central, p, and q coordinates of Base in separate files
+template <typename T>
+void save_coordinates(std::vector<Base<T>> const& dna) {
+  std::ofstream c_file("./file_c.txt");
+  std::ofstream p_file("./file_p.txt");
+  std::ofstream q_file("./file_q.txt");
+
+  if (c_file.is_open() && p_file.is_open() && q_file.is_open()) {
+    c_file << std::fixed << std::setprecision(6);
+    p_file << std::fixed << std::setprecision(6);
+    q_file << std::fixed << std::setprecision(6);
+    for (auto const& b : dna) {
+      c_file << b.central().x_ << '\t' << b.central().y_ << '\t' << b.central().z_ << '\n';
+      p_file << b.p().x_ << '\t' << b.p().y_ << '\t' << b.p().z_ << '\n';
+      q_file << b.q().x_ << '\t' << b.q().y_ << '\t' << b.q().z_ << '\n';
+    }
+    c_file.close();
+    p_file.close();
+    q_file.close();
+  } else {
+    std::cerr << "Failed to open file for writing.\n";
+  }
 }
