97a0fac97443ba7a0a263e89f06838b6e79404dc
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-04 10:32:18 +0100

    simplest main
    this is the easyest program with the model, without Metropolis algorithm in the lowest energy condiction

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,10 @@
 #include <iomanip>
 #include <random>
 
-#include "tqdm.hpp"
+#include "parameters.hpp"
 #include "coordinates.hpp"
 #include "base.hpp"
 #include "functions.hpp"
-
-// evolution steps
-int constexpr epochs = 100000;
 
 // number of basis
 int constexpr n = 100;
@@ -38,46 +35,7 @@ int main() {
     dna.push_back(b);
   }
   double energy = calculate_energy(dna);
+  std::cout << "Energy:\t" << energy << '\n';
 
-  for (int e : tq::trange(epochs)) {
-    std::vector<Base<double>> dna_new{dna[0], dna[1]};
-    previous_rotation_matrix = MatrixXd::Identity(3, 3);
-    central_previous_coordinates = dna_new[1].central();
-    for (int i = 2; i != n; ++i) {
-      double const theta = dna[i].theta() + oscillation(gen) * step;
-      double const phi = dna[i].phi() + oscillation(gen) * step;
-      double const psi = dna[i].psi() + oscillation(gen) * step;
-      Base<double> b(theta, phi, psi);
-      previous_rotation_matrix = b.calculate_coordinates(previous_rotation_matrix,
-                                      central_previous_coordinates);
-      central_previous_coordinates = b.central();
-      dna_new.push_back(b);
-    }
-    double const energy_new = calculate_energy(dna_new);
-    if (p(energy_new, energy) > uniform(gen)) {
-      dna = std::move(dna_new);
-      energy = energy_new;
-    }
-  }
-
-  // Save central, p, and q coordinates of Base in separate files
-  std::ofstream c_file("./file_c.txt");
-  std::ofstream p_file("./file_p.txt");
-  std::ofstream q_file("./file_q.txt");
-
-  if (c_file.is_open() && p_file.is_open() && q_file.is_open()) {
-    c_file << std::fixed << std::setprecision(6);
-    p_file << std::fixed << std::setprecision(6);
-    q_file << std::fixed << std::setprecision(6);
-    for (auto const& b : dna) {
-      c_file << b.central().x_ << '\t' << b.central().y_ << '\t' << b.central().z_ << '\n';
-      p_file << b.p().x_ << '\t' << b.p().y_ << '\t' << b.p().z_ << '\n';
-      q_file << b.q().x_ << '\t' << b.q().y_ << '\t' << b.q().z_ << '\n';
-    }
-    c_file.close();
-    p_file.close();
-    q_file.close();
-  } else {
-    std::cerr << "Failed to open file for writing.\n";
-  }
+  save_coordinates(dna);
 }
