e1ea3a061d3134a26c427926e2c1959bcf282d9d
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-05 17:07:00 +0100

    to calculate chirality
    add empty constructor,operator % (vector product) and operator +=. Template in operator / for the denominator

--- src/coordinates.hpp
+++ src/coordinates.hpp
@@ -10,13 +10,15 @@ struct Coordinates {
   T y_;
   T z_;
 
+  Coordinates() : x_(0), y_(0), z_(0) {}
   Coordinates(T x, T y, T z) : x_(x), y_(y), z_(z) {}
 
   Coordinates<T> operator-(Coordinates<T> const& c) const {
     return {x_ - c.x_, y_ - c.y_, z_ - c.z_};
   }
 
-  Coordinates<T> operator/(T const& d) const {
+  template<typename U>
+  Coordinates<T> operator/(U const& d) const {
     return {x_ / d, y_ / d, z_ / d};
   }
 
@@ -27,6 +29,19 @@ struct Coordinates {
   T operator&&(Coordinates<T> const& c) const {
     return std::sqrt(std::pow(x_ - c.x_, 2) + std::pow(y_ - c.y_, 2)
                       + std::pow(z_ - c.z_, 2));
+  }
+
+  template<typename U>
+  Coordinates<T> operator%(Coordinates<U> const& c) const {
+    return {y_ * c.z_ - z_ * c.y_, z_ * c.x_ - x_ * c.z_, x_ * c.y_ - y_ * c.x_};
+  }
+
+  template<typename U>
+  Coordinates<T>& operator+=(Coordinates<U> const& c) {
+    x_ += c.x_;
+    y_ += c.y_;
+    z_ += c.z_;
+    return *this;
   }
 
   void print() const {
