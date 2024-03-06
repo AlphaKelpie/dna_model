2cbbbd50d23f9b3ed63c5bf083d232d98460c1ba
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-06 10:08:14 +0100

    not && operator but ||. Add .nomr() method. Add comment and template in -

--- src/coordinates.hpp
+++ src/coordinates.hpp
@@ -13,35 +13,47 @@ struct Coordinates {
   Coordinates() : x_(0), y_(0), z_(0) {}
   Coordinates(T x, T y, T z) : x_(x), y_(y), z_(z) {}
 
-  Coordinates<T> operator-(Coordinates<T> const& c) const {
+  // subtraction of coordinates
+  template<typename U>
+  Coordinates<T> operator-(Coordinates<U> const& c) const {
     return {x_ - c.x_, y_ - c.y_, z_ - c.z_};
   }
 
+  // scalar division
   template<typename U>
   Coordinates<T> operator/(U const& d) const {
     return {x_ / d, y_ / d, z_ / d};
   }
 
+  // scalar product
   T operator*(Coordinates<T> const& c) const {
     return x_ * c.x_ + y_ * c.y_ + z_ * c.z_;
   }
 
-  T operator&&(Coordinates<T> const& c) const {
+  // norm of substracted coordinates
+  T operator||(Coordinates<T> const& c) const {
     return std::sqrt(std::pow(x_ - c.x_, 2) + std::pow(y_ - c.y_, 2)
                       + std::pow(z_ - c.z_, 2));
   }
 
+  // cross product
   template<typename U>
   Coordinates<T> operator%(Coordinates<U> const& c) const {
     return {y_ * c.z_ - z_ * c.y_, z_ * c.x_ - x_ * c.z_, x_ * c.y_ - y_ * c.x_};
   }
 
+  // addition of coordinates
   template<typename U>
   Coordinates<T>& operator+=(Coordinates<U> const& c) {
     x_ += c.x_;
     y_ += c.y_;
     z_ += c.z_;
     return *this;
+  }
+
+  // norm of coordinates
+  T norm() const {
+    return std::sqrt(x_ * x_ + y_ * y_ + z_ * z_);
   }
 
   void print() const {
