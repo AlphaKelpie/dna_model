c14d3f281576354ac6505d38a3b7d13c73628a97
Tommaso Rondini <tommaso.rondini@studio.unibo.it>
2024-03-07 13:43:29 +0100

    initial commit
    function to send email

---
+++ src/send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+
+def send_email(subject, body, user="send_from@gmail.com", pwd="password", recipient="send_to@email.com"):
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print('successfully sent the mail')
+    except:
+        print("failed to send mail")
